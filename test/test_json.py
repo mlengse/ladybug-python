0a6afe191517ceacccde6ee27069c406fbaf938b
Arun Sharma <arun@sharma-home.net>
2026-05-27 18:27:27 -0700

    Revert "Statically link JSON extension in pybind CI"
    This reverts commit 13d6905594093172d554eb37fda77eda894b7d46.

--- test/test_json.py
+++ test/test_json.py
@@ -11,6 +11,8 @@ def test_to_json_string_param_roundtrip(conn_db_empty: ConnDB) -> None:
     """to_json() with a JSON string parameter should store the parsed object, not a string literal."""
     conn, _ = conn_db_empty
     conn.execute("""
+        INSTALL json;
+        LOAD json;
         CREATE NODE TABLE User (id SERIAL PRIMARY KEY, meta JSON);
         """)
 
